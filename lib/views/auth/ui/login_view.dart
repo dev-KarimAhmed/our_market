94d2028e7a1f7c22ca6183df9c1f48aba3606c7c
Karim Ahmed <devKarimAhmed@gmail.com>
2024-09-23 02:36:42 +0300

    create custom text button

--- lib/views/auth/ui/login_view.dart
+++ lib/views/auth/ui/login_view.dart
@@ -54,12 +54,48 @@ class LoginView extends StatelessWidget {
                       icon: const Icon(Icons.visibility_off),
                     ),
                   ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      CustomTextButton(
+                        text: "Forgot Password?",
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
           )
         ],
       )),
+    );
+  }
+}
+
+class CustomTextButton extends StatelessWidget {
+  const CustomTextButton({
+    super.key,
+    this.onTap,
+    required this.text,
+  });
+  final void Function()? onTap;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: AppColors.kPrimaryColor,
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
+      ),
     );
   }
 }
