d5fb529c71f03dc146aefeee23e754a6ea579c99
Karim Ahmed <devKarimAhmed@gmail.com>
2024-09-28 17:09:14 +0300

    add categories list to home view

--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -16,8 +16,62 @@ class HomeView extends StatelessWidget {
             height: 20,
           ),
           Image.asset("assets/images/buy.jpg"),
+          const SizedBox(
+            height: 15,
+          ),
+          const Text(
+            "Popular Categories",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: AppColors.kPrimaryColor,
+                        foregroundColor: AppColors.kWhiteColor,
+                        child: Icon(
+                          categories[index].icon,
+                          size: 40,
+                        ),
+                      ),
+                      Text(categories[index].text),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
   }
+}
+
+List<Category> categories = [
+  Category(icon: Icons.sports, text: "Sports"),
+  Category(icon: Icons.tv, text: "Electronics"),
+  Category(icon: Icons.collections, text: "Collections"),
+  Category(icon: Icons.book, text: "Books"),
+  Category(icon: Icons.games, text: "Games"),
+  Category(icon: Icons.bike_scooter, text: "Bikes"),
+];
+
+class Category {
+  final IconData icon;
+  final String text;
+
+  Category({required this.icon, required this.text});
 }
