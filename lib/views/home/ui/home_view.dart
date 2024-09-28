5ba6928af3a1d8b1c8ea4d13cfd7c39803cc2c65
Karim Ahmed <devKarimAhmed@gmail.com>
2024-09-28 17:13:18 +0300

    refactor for categories list

--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:our_market/core/app_colors.dart';
 import 'package:our_market/core/components/custom_search_field.dart';
+import 'package:our_market/views/home/ui/widgets/categories_list.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -28,50 +29,9 @@ class HomeView extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          SizedBox(
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: AppColors.kPrimaryColor,
-                        foregroundColor: AppColors.kWhiteColor,
-                        child: Icon(
-                          categories[index].icon,
-                          size: 40,
-                        ),
-                      ),
-                      Text(categories[index].text),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
+          const CategoriesList(),
         ],
       ),
     );
   }
-}
-
-List<Category> categories = [
-  Category(icon: Icons.sports, text: "Sports"),
-  Category(icon: Icons.tv, text: "Electronics"),
-  Category(icon: Icons.collections, text: "Collections"),
-  Category(icon: Icons.book, text: "Books"),
-  Category(icon: Icons.games, text: "Games"),
-  Category(icon: Icons.bike_scooter, text: "Bikes"),
-];
-
-class Category {
-  final IconData icon;
-  final String text;
-
-  Category({required this.icon, required this.text});
 }
