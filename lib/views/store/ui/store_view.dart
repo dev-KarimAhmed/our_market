aa4d95f05238dfb13176b368251d06efa61fad6a
Karim Ahmed <devKarimAhmed@gmail.com>
2024-11-18 23:02:24 +0200

    enable search in store view

--- lib/views/store/ui/store_view.dart
+++ lib/views/store/ui/store_view.dart
@@ -1,17 +1,29 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:our_market/core/components/custom_search_field.dart';
 import 'package:our_market/core/components/products_list.dart';
 
-class StoreView extends StatelessWidget {
+import '../../../core/functions/navigate_to.dart';
+import '../../home/ui/search_view.dart';
+
+class StoreView extends StatefulWidget {
   const StoreView({super.key});
+
+  @override
+  State<StoreView> createState() => _StoreViewState();
+}
+
+class _StoreViewState extends State<StoreView> {
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ListView(
-        children: const [
-          Center(
+        children: [
+          const Center(
             child: Text(
               "Welcome To Our Market",
               style: TextStyle(
@@ -20,16 +32,35 @@ class StoreView extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
-          CustomSearchField(),
-          SizedBox(
+          CustomSearchField(
+            controller: _searchController,
+            onPressed: () {
+              if (_searchController.text.isNotEmpty) {
+                navigateTo(
+                  context,
+                  SearchView(
+                    query: _searchController.text,
+                  ),
+                );
+                _searchController.clear();
+              }
+            },
+          ),
+          const SizedBox(
             height: 15,
           ),
-          ProductsList()
+          const ProductsList()
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 }
